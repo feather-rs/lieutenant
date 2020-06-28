98ff911186b880c409d894a1dfc40f7136459149
Buggstream <buggstream@tuta.io>
2020-06-28 15:40:13 +0200

    Fix advance_to_end not consuming the input str

--- src/parser.rs
+++ src/parser.rs
@@ -22,7 +22,9 @@ impl<'a> Input<'a> {
     /// Advances until the end of input, returning all
     /// consumed characters.
     pub fn advance_to_end(&mut self) -> &'a str {
-        self.ptr
+        let consumed = self.ptr;
+        self.ptr = &self.ptr[self.ptr.len()..];
+        consumed
     }
 
     /// Returns the number of remaining characters to read.
